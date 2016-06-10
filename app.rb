10aed716ae4db507daec04d7cf57501c441de8c7
danvine <github@danvine.com>
2016-06-10 18:28:13 -0400

    fix endpoint

--- app.rb
+++ app.rb
@@ -40,10 +40,8 @@ class Hackrss < Sinatra::Base
       options = {
         url: url,
         viewport: "1024x600",
-        fullpage: false,
         thumbnail_max_width: 300,
         alt: item[:title],
-        unique: params[:unique],
         base: '104.154.114.8/'
       }
 
